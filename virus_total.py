5dad2996de8b58e43601c70f847be4b94f913c14
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2022-07-23 20:12:07 +0430

    Check if scan of a file exists before uploading

--- virus_total.py
+++ virus_total.py
@@ -1,15 +1,21 @@
 import vt
+from vt import APIError
 
 
-def scan_and_fetch_analysis_stats(file_path_list: list[str]):
+def scan_and_fetch_analysis_stats(path, sha):
     client = vt.Client('TOKEN')
-    for path in file_path_list:
-        with open(path, "rb") as f:
-            print("scanning")
-            analysis = client.scan_file(f, wait_for_completion=True)
-            print("completed")
-            f.close()
-        completed_analysis = client.get_object(f"/analyses/{analysis.id}")
-        with(open('analysis.txt', "a+")) as analysis_file:
-            analysis_file.write(f"{path} {completed_analysis.stats}")
+    try:
+        file_info = client.get_object(
+            f"/files/{sha}")
+        return
+    except APIError:
+        print("file does not exist")
+    with open(path, "rb") as f:
+        print("scanning")
+        analysis = client.scan_file(f, wait_for_completion=True)
+        print("completed")
+        f.close()
+    completed_analysis = client.get_object(f"/analyses/{analysis.id}")
+    with(open("analysis.txt", "a+")) as analysis_file:
+        analysis_file.write(f"{path} {completed_analysis.stats}")
     client.close()
