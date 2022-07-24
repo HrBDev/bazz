f936dc69e9f7255442062f7683db6db328fec1e8
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2022-07-24 22:32:39 +0430

    Add myket sitemap

--- app_list.py
+++ app_list.py
@@ -1,34 +1,42 @@
 from xml.dom import minidom
+
 import requests
 
-urls = []
-res = requests.get('https://cafebazaar.ir/app-sitemap.xml')
-xmldoc = minidom.parseString(res.text)
-itemlist = xmldoc.getElementsByTagName('loc')
-for s in itemlist:
-    #if str(s.childNodes[0].nodeValue).split('/')[3].split('-')[2] == 'fa':
-        print(s.childNodes[0].nodeValue)
-        urls.append(s.childNodes[0].nodeValue)
 
-apps = []
-
-for url in urls:
-    print(url)
-    try:
-        res = requests.get(url)
-    except requests.exceptions.ConnectionError:
-        try:
-            while res.status_code != 200:
-                res = requests.get(url)
-        except:
-            pass
-    print(res.status_code)
+def save_app_list(sitemap):
+    urls = []
+    res = requests.get(sitemap)
     xmldoc = minidom.parseString(res.text)
     itemlist = xmldoc.getElementsByTagName('loc')
     for s in itemlist:
-        #print((s.childNodes[0].nodeValue).split('/')[4])
-        apps.append((s.childNodes[0].nodeValue).split('/')[4])
-with open("apps.txt", "a") as myfile:
-    for app in apps:
-        myfile.write(app)
-        myfile.write('\n')
+        # if str(s.childNodes[0].nodeValue).split('/')[3].split('-')[2] == 'fa':
+        print(s.childNodes[0].nodeValue)
+        urls.append(s.childNodes[0].nodeValue)
+
+    apps = []
+
+    for url in urls:
+        print(url)
+        try:
+            res = requests.get(url.strip())
+        except requests.exceptions.ConnectionError:
+            try:
+                while res.status_code != 200:
+                    res = requests.get(url)
+            except:
+                pass
+        print(res.status_code)
+        xmldoc = minidom.parseString(res.text)
+        itemlist = xmldoc.getElementsByTagName('loc')
+        for s in itemlist:
+            # print((s.childNodes[0].nodeValue).split('/')[4])
+            apps.append(s.childNodes[0].nodeValue.split('/')[4])
+    with open(f"{sitemap.split('/')[2].split('.')[0]}_apps.txt", "a") as myfile:
+        for app in apps:
+            myfile.write(app)
+            myfile.write('\n')
+
+
+if __name__ == '__main__':
+    save_app_list('https://cafebazaar.ir/app-sitemap.xml')
+    save_app_list('https://myket.ir/sitemap.xml')
