84820a64bf2ee63662aed9999731a20676bf32d0
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2022-07-25 03:03:42 +0430

    Don't hardcode stuff

--- app_list.py
+++ app_list.py
@@ -7,7 +7,7 @@ def save_app_list(sitemap):
     urls = []
     res = requests.get(sitemap)
     xmldoc = minidom.parseString(res.text)
-    itemlist = xmldoc.getElementsByTagName('loc')
+    itemlist = xmldoc.getElementsByTagName("loc")
     for s in itemlist:
         # if str(s.childNodes[0].nodeValue).split('/')[3].split('-')[2] == 'fa':
         print(s.childNodes[0].nodeValue)
@@ -27,16 +27,15 @@ def save_app_list(sitemap):
                 pass
         print(res.status_code)
         xmldoc = minidom.parseString(res.text)
-        itemlist = xmldoc.getElementsByTagName('loc')
+        itemlist = xmldoc.getElementsByTagName("loc")
         for s in itemlist:
-            # print((s.childNodes[0].nodeValue).split('/')[4])
-            apps.append(s.childNodes[0].nodeValue.split('/')[4])
-    with open(f"{sitemap.split('/')[2].split('.')[0]}_apps.txt", "a") as myfile:
+            apps.append(s.childNodes[0].nodeValue.split('/')[4].strip())
+    with open(f"{sitemap.split('/')[2].split('.')[0]}_apps.txt", "a") as f:
         for app in apps:
-            myfile.write(app)
-            myfile.write('\n')
+            f.write(app)
+            f.write('\n')
 
 
 if __name__ == '__main__':
-    save_app_list('https://cafebazaar.ir/app-sitemap.xml')
-    save_app_list('https://myket.ir/sitemap.xml')
+    save_app_list("https://cafebazaar.ir/app-sitemap.xml")
+    save_app_list("https://myket.ir/sitemap.xml")
--- utils.py
+++ utils.py
@@ -3,9 +3,15 @@ import json
 import logging
 import os
 import traceback
+from enum import Enum
 
 import requests
 from joblib import Parallel, delayed
+
+
+class Market(Enum):
+    BAZAAR = 1
+    MYKET = 2
 
 
 def get_download_link(pkg_name: str) -> str:
@@ -42,8 +48,11 @@ def get_download_link(pkg_name: str) -> str:
     return f"https://appcdn.cafebazaar.ir/apks/{response_json['singleReply']['appDownloadInfoReply']['token']}"
 
 
-def save_apk_to_file(pkg_name: str):
-    url = get_download_link(pkg_name)
+def save_apk_to_file(pkg_name: str, market: Market):
+    if market == Market.BAZAAR:
+        url = get_download_link(pkg_name)
+    else:
+        url = f"https://myket.ir/get/app?packageName={pkg_name}"
     response = requests.get(url)
     with open(f"{pkg_name}.apk", "wb") as f:
         f.write(response.content)
@@ -54,37 +63,37 @@ def get_sha256(path: str) -> str:
         return hashlib.sha256(f.read()).hexdigest()
 
 
-def download_calculate_sha_and_save():
-    with open("apps.txt") as f:
+def download_calculate_sha_and_save(path: str, market: Market):
+    with open(path) as f:
         pkgs = f.readlines()
         for line in pkgs:
-            process(line)
+            process(line, market)
 
 
-def download_calculate_sha_and_save_parallel():
-    with open("apps.txt") as f:
+def download_calculate_sha_and_save_parallel(path: str, market: Market):
+    with open(path) as f:
         pkgs = f.readlines()
         f.close()
-    Parallel(n_jobs=8)(delayed(process)(line) for line in pkgs)
+    Parallel(n_jobs=8)(delayed(process)(line.strip(), market) for line in pkgs)
 
 
-def process(line):
-    stripped_line = line.strip()
-    logging.warning(f"Processing {stripped_line}")
+def process(line: str, market: Market):
+    logging.warning(f"Processing {line}")
     try:
-        save_apk_to_file(stripped_line)
-        sha = get_sha256(f"./{stripped_line}.apk")
+        save_apk_to_file(line, market)
+        sha = get_sha256(f"./{line}.apk")
         with open("sha_list.txt", "a+") as output:
-            output.write(f"{stripped_line} {sha}\n")
+            output.write(f"{line} {sha}\n")
             output.close()
-        os.remove(f"./{stripped_line}.apk")
+        os.remove(f"./{line}.apk")
         return sha
     except TypeError:
         pass
     except Exception:
-        logging.error(f"Error on {stripped_line}")
+        logging.error(f"Error on {line}")
         logging.error(traceback.format_exc())
 
 
 if __name__ == '__main__':
-    download_calculate_sha_and_save_parallel()
+    download_calculate_sha_and_save_parallel("cafebazaar_apps.txt", Market.BAZAAR)
+    # download_calculate_sha_and_save_parallel("myket_apps.txt", Market.MYKET)
