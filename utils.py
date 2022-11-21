0f816209b5ff612bd9343f2491af99c4b50c6b7e
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2022-11-21 09:00:39 +0000

    Pending changes exported from your codespace

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import logging
 import os
 import traceback
 from enum import Enum
-
+from bs4 import BeautifulSoup
 import requests
 from joblib import Parallel, delayed
 from sqlalchemy import create_engine, insert
@@ -77,7 +77,7 @@ def download_calculate_sha_and_save_parallel(path: str, market: Market):
     with open(path) as f:
         pkgs = f.readlines()
         f.close()
-    Parallel(n_jobs=8)(delayed(process)(line.strip(), market) for line in pkgs)
+    Parallel(n_jobs=6)(delayed(process)(line.strip(), market) for line in pkgs)
 
 
 def process(line: str, market: Market):
@@ -90,15 +90,41 @@ def process(line: str, market: Market):
         with engine.connect().execution_options(isolation_level="AUTOCOMMIT") as conn:
             conn.execute(stmt)
             conn.close()
-        os.remove(f"./{line}.apk")
-        return sha
-    except TypeError:
-        pass
     except Exception:
         logging.error(f"Error on {line}")
-        logging.error(traceback.format_exc())
+    finally:
+        try:
+            os.remove(f"./{line}.apk")
+        except:
+            pass
 
+def is_available():
+    with open("diff.txt") as f:
+        diff = f.readlines()
+        f.close()
+    for pkg_name in diff:
+        req = requests.get(f"https://cafebazaar.ir/app/{pkg_name}")
+        soup = BeautifulSoup(req.content, "html.parser")
+        error = soup.find("div", {"data-status": "500"})
+        try:
+            banned = soup.find("h2", {
+                "class": "fs-12 AppSubtitles__error"}).text == "این برنامه به علت عدم رعایت قوانین کافه بازار از حالت " \
+                                                            "انتشار خارج شده است "
+        except AttributeError:
+            banned = None
+        paid = soup.find("div", {"fs-12 AppSubtitles__item"})
+        if error is None and banned is None and paid is None:
+            print("available")
+            with open("diff_cleaned.txt", "a+") as f:
+                f.write(pkg_name)
+        if error is not None:
+            print("error")
+        if banned is not None:
+            print("banned")
+        if paid is not None:
+            print("paid")
 
 if __name__ == '__main__':
-    download_calculate_sha_and_save_parallel("cafebazaar_apps.txt", Market.BAZAAR)
+    is_available()
+    # download_calculate_sha_and_save_parallel("cafebazaar_apps.txt", Market.BAZAAR)
     # download_calculate_sha_and_save_parallel("myket_apps.txt", Market.MYKET)
